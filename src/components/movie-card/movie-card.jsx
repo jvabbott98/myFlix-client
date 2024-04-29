6e20b7ce6adbaad6dd6ffce73d4706b90ff589bf
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-04-29 10:58:32 -0400

    routing

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
@@ -8,18 +10,18 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.director}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant="link">
-          Open
-        </Button>
+        <Link to={`/movies/${encodeURICOmponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
 };
 
-  MovieCard.propTypes = {
-    movie: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      director: PropTypes.string
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
-  };
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    director: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,5 @@
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import './movie-view.scss';
 
 export const MovieView = ({ movie, onBackClick }) => {
